72fad7dd3b8b233fb968a0dd4ac2fd92b636645c
Djordje Maslic <djooordje@gmail.com>
2023-08-16 14:06:44 +0200

    new link in auth repository

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -304,9 +304,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "c97716503b30baa74ffe9c45d3987b1e
 "icons/Icon-512.png": "8db20f1c850bc204f3ad89cfbefd3eb6",
 "icons/Icon-maskable-192.png": "322f38fdde3aefa5dfc7100e36cb143a",
 "icons/Icon-maskable-512.png": "8db20f1c850bc204f3ad89cfbefd3eb6",
-"index.html": "ffa5b23e175a1044be2a08110d62a387",
-"/": "ffa5b23e175a1044be2a08110d62a387",
-"main.dart.js": "0b1210d8b205933aa9508f1d9a20147f",
+"index.html": "19ab905b7829edfedbfe052f57a821b7",
+"/": "19ab905b7829edfedbfe052f57a821b7",
+"main.dart.js": "e93d75894e7947ae64fd5b4313a475a4",
 "manifest.json": "feef6db0c0257c0329e4f6349069eba8",
 "version.json": "39a8abb54c85fc7c16d9d5dd20c896e9"};
 // The application shell files that are downloaded before a service worker can
