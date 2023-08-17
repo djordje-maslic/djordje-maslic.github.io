c0f096bbab2d9131e6778e0260ba0addfc4c0230
Djordje Maslic <djooordje@gmail.com>
2023-08-17 11:45:21 +0200

    dynamic link domain added

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -304,9 +304,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "c97716503b30baa74ffe9c45d3987b1e
 "icons/Icon-512.png": "8db20f1c850bc204f3ad89cfbefd3eb6",
 "icons/Icon-maskable-192.png": "322f38fdde3aefa5dfc7100e36cb143a",
 "icons/Icon-maskable-512.png": "8db20f1c850bc204f3ad89cfbefd3eb6",
-"index.html": "7fd27cd1720802f6c358c47805b9f85f",
-"/": "7fd27cd1720802f6c358c47805b9f85f",
-"main.dart.js": "417a1613210a27ddd9b90eac8734e09a",
+"index.html": "15050152c104b22ed37a1031f67ab416",
+"/": "15050152c104b22ed37a1031f67ab416",
+"main.dart.js": "1d904bd05291fe6abbf338be45190e8f",
 "manifest.json": "feef6db0c0257c0329e4f6349069eba8",
 "version.json": "39a8abb54c85fc7c16d9d5dd20c896e9"};
 // The application shell files that are downloaded before a service worker can
