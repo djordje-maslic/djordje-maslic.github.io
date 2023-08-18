a7aa29400337ffde9d392e38dd7bf7d238426c06
Djordje Maslic <djooordje@gmail.com>
2023-08-18 10:55:03 +0200

    hive error solved

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -304,9 +304,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "c97716503b30baa74ffe9c45d3987b1e
 "icons/Icon-512.png": "8db20f1c850bc204f3ad89cfbefd3eb6",
 "icons/Icon-maskable-192.png": "322f38fdde3aefa5dfc7100e36cb143a",
 "icons/Icon-maskable-512.png": "8db20f1c850bc204f3ad89cfbefd3eb6",
-"index.html": "361d6186c1de2c7f11dbea81a1f13d09",
-"/": "361d6186c1de2c7f11dbea81a1f13d09",
-"main.dart.js": "a6ccb43016b53fd0c6729455417c9134",
+"index.html": "58eb9e2261ef69a5794a1679f256907e",
+"/": "58eb9e2261ef69a5794a1679f256907e",
+"main.dart.js": "85afc77d737be49bd02ccdda1eb65f84",
 "manifest.json": "feef6db0c0257c0329e4f6349069eba8",
 "version.json": "39a8abb54c85fc7c16d9d5dd20c896e9"};
 // The application shell files that are downloaded before a service worker can
