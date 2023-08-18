29e39b03a13b6f0de37c62e2892306f482afe6c9
Djordje Maslic <djooordje@gmail.com>
2023-08-18 10:21:41 +0200

    Hive added

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -304,9 +304,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "c97716503b30baa74ffe9c45d3987b1e
 "icons/Icon-512.png": "8db20f1c850bc204f3ad89cfbefd3eb6",
 "icons/Icon-maskable-192.png": "322f38fdde3aefa5dfc7100e36cb143a",
 "icons/Icon-maskable-512.png": "8db20f1c850bc204f3ad89cfbefd3eb6",
-"index.html": "866cbb2d88952313a6468c23a3be4663",
-"/": "866cbb2d88952313a6468c23a3be4663",
-"main.dart.js": "2385aa01a9aa4ced672730c6af896d3e",
+"index.html": "df97ce7c959a4f61c0bcef80a539c728",
+"/": "df97ce7c959a4f61c0bcef80a539c728",
+"main.dart.js": "a6ccb43016b53fd0c6729455417c9134",
 "manifest.json": "feef6db0c0257c0329e4f6349069eba8",
 "version.json": "39a8abb54c85fc7c16d9d5dd20c896e9"};
 // The application shell files that are downloaded before a service worker can
